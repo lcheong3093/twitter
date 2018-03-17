94ce7448e67fd887e24fe86eb3398ec93d41b50a
Lauren Cheong <lauren.cheong@stonybrook.edu>
2018-03-16 22:50:17 -0400

    Verifies & returns status (script)

--- routes/server.js
+++ routes/server.js
@@ -5,12 +5,15 @@ var mongo = require('mongodb');
 var mongoClient = mongo.MongoClient;
 var url = "mongodb://localhost"
 
+var nodemailer = require('nodemailer');
+var rand = require('generate-key');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('welcome');
 });
 
+/* Create Account */
 router.post('/adduser', function(req, res) {
   var username = req.body.username.toLowerCase();
   var email = req.body.email.toLowerCase();
@@ -18,25 +21,44 @@ router.post('/adduser', function(req, res) {
 
   console.log("User: " + username + " Email: " + email + " Pass: " + password);
 
-  checkEmail(email, username, function(err, string){
-    if(err) throw err;
+  checkInfo(email, username, function(err, string){
+    if(err){ 
+      throw err;
+      // res.send({status: "error"});
+    }
 
-    if(string !== undefined)
-      res.send(string);
-    else{
+    if(string !== undefined){
+      // res.send(string);
+      res.send({status: "error"});
+    }else{
       var user = {email: email, username: username, password: password, status: "unverified"};
-      addNewUser(user);
+      addNewUser(user, function(err, email){
+        var key = rand.generateKey();
+        sendVerification(email, key);
+        // res.render('verify', {key: key, username: username});
+        res.send({status: "OK"});
+      });
     }
   });
 
 });
 
+/* Verify Account */
 router.post('/verify', function(req, res) {
-  
+  if(req.body.key !== req.body.verification){
+    // res.render('verifyerror', {key: req.body.key, username: req.body.username});
+    res.send({status: "error"});
+  }else{
+    verifyUser(req.body.username);
+    // res.render('login');
+    res.send({status: "OK"});
+  }
 }); 
 
-/*Checks for unique email*/
-function checkEmail(email, username, callback){
+/*** HELPERS ***/
+
+//Check for unique email & username
+function checkInfo(email, username, callback){
   console.log("checkEmail: " + email);
   mongoClient.connect(url, function(err, db) {
     if (err) throw err;
@@ -67,13 +89,58 @@ function checkEmail(email, username, callback){
   });
 }
 
-function addNewUser(user){
+//Add user to database
+function addNewUser(user, callback){
   mongoClient.connect(url, function(err, db) {
 		if (err) throw err;		
 		var twitter = db.db("twitter");
 		twitter.collection("users").insertOne(user, function(err, res) {
 			if (err) throw err;
-			console.log("New user added to database: ", user);
+      console.log("New user added to database: ", user);
+      callback(err, user.email);
+			db.close();
+		});
+	});
+}
+
+//Send verification email w/ key
+function sendVerification(email, key){
+  var message = "validation key: " + key;
+
+  const transport = nodemailer.createTransport({
+		host: 'smtp.gmail.com',
+		port: 465,
+		secure: true,
+		auth: {
+		  user: 'twittercloud356@gmail.com',
+		  pass: 'twoCOMESafterONE'
+		}
+  });
+  
+	var mailOpts = {
+        from: 'user@gmail.com',
+        to: email,
+        subject: 'Verify your account',
+        text: message
+  };
+  
+	transport.sendMail(mailOpts, (err, info) => {
+		if (err) console.log(err); //Handle Error
+		console.log(info);
+  });
+}
+
+//Update user's status to verified
+function verifyUser(username){
+  mongoClient.connect(url, function(err, db) {
+    if (err) throw err;		
+    
+    var twitter = db.db("twitter");
+    var newvalues = { $set: { status: "verified" } };
+		twitter.collection("users").updateOne({username: username}, newvalues, function(err, res) {
+      if (err) throw err;
+      
+      console.log("Verified user and updated db: ", username);
 			db.close();
 		});
 	});
