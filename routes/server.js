92b383023d937c56260187326ff676b76c52fe1b
Lauren Cheong <lauren.cheong@stonybrook.edu>
2018-03-21 19:33:27 -0400

    abracadabra

--- routes/server.js
+++ routes/server.js
@@ -56,7 +56,8 @@ router.post('/adduser', function(req, res) {
 
 /* Verify Account */
 router.post('/verify', function(req, res) {
-  if(req.body.key !== req.body.verification || req.body.key !== "abracadabra"){
+  console.log("correct key: " + req.body.key + " user key: " + req.body.verification);
+  if(req.body.key !== req.body.verification || req.body.verification !== "abracadabra"){
     // res.render('verifyerror', {key: req.body.key, username: req.body.username});
     res.send({status: "error"});
   }else{
