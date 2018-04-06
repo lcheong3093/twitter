ee69af90a9e248c6f28a458d09d2190f629fb567
Lauren Cheong <lauren.cheong@stonybrook.edu>
2018-04-06 00:34:22 -0400

    search

--- routes/server.js
+++ routes/server.js
@@ -226,13 +226,13 @@ router.post('/search', function(req, res){
     for(var field in req.body){
       if(req.body[field] !== "" && field !== "limit" && field !== "following"){ //Add given queries into query
         if(field === "timestamp"){
-          query[field] = {$gte: field};
+          query[field] = {$lte: field};
         }else{
           query[field] = req.body[field];
         }
       }else{
         if(field === "timestamp"){
-          query[field] = {$gte: timestamp};
+          query[field] = {$lte: timestamp};
         }
       }
     }
